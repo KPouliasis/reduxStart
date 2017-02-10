d5b443610292f8058192ea18e9dcd1651f934246
Benjamin <benjamin.gu100@gmail.com>
2017-02-10 15:22:11 -0500

    up to middleware

--- browser/react/index.js
+++ browser/react/index.js
@@ -9,7 +9,7 @@ import Artist from './components/Artist';
 import Songs from './components/Songs';
 import NewPlaylistContainer from './containers/NewPlaylistContainer';
 import Playlist from './components/Playlist';
-
+import LyricsContainer from './containers/LyricsContainer';
 import store from './store';
 import setLyrics from './action-creators/lyrics';
 
@@ -39,6 +39,7 @@ ReactDOM.render(
       </Route>
       <Route path="/new-playlist" component={NewPlaylistContainer} />
       <Route path="playlists/:playlistId" component={Playlist} />
+      <Route path="/lyrics" component={LyricsContainer} />
       <IndexRedirect to='/albums' />
     </Route>
   </Router>,
